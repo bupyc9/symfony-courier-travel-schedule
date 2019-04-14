6fa538c7edba98a9faf8e1a935254b0ff7364699
Pavel Afanasev <bupyc9@gmail.com>
2019-04-14 17:23:29 +0300

    Added command 'app:couriers'

---
+++ source/src/Command/CouriersCommand.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Courier;
+use App\Entity\Region;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class CouriersCommand extends Command
+{
+    protected static $defaultName = 'app:couriers';
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+
+        $this->entityManager = $entityManager;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Add couriers');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $items = $this->getItems();
+        $repository = $this->entityManager->getRepository(Courier::class);
+        $count = 0;
+        foreach ($items as $item) {
+            $courier = $repository->findOneBy(
+                [
+                    'firstName' => $item['firstName'],
+                    'lastName' => $item['lastName'],
+                    'secondName' => $item['secondName'],
+                ]
+            );
+            if (null !== $courier) {
+                continue;
+            }
+
+            $courier = new Courier();
+            $courier
+                ->setFirstName($item['firstName'])
+                ->setLastName($item['lastName'])
+                ->setSecondName($item['secondName']);
+            $this->entityManager->persist($courier);
+            ++$count;
+        }
+
+        $this->entityManager->flush();
+
+        $io->success("Added {$count} couriers");
+    }
+
+    protected function getItems(): array
+    {
+        return [
+            ['firstName' => 'Прохор', 'lastName' => 'Тимофеев', 'secondName' => 'Германович'],
+            ['firstName' => 'Юстиниан', 'lastName' => 'Белов', 'secondName' => 'Гордеевич'],
+            ['firstName' => 'Лукьян', 'lastName' => 'Филатов', 'secondName' => 'Александрович'],
+            ['firstName' => 'Эрнест', 'lastName' => 'Фёдоров', 'secondName' => 'Антонинович'],
+            ['firstName' => 'Анатолий', 'lastName' => 'Рогов', 'secondName' => 'Яковович'],
+            ['firstName' => 'Варлаам', 'lastName' => 'Петухов', 'secondName' => 'Германович'],
+            ['firstName' => 'Аполлон', 'lastName' => 'Капустин', 'secondName' => 'Ярославович'],
+            ['firstName' => 'Захар', 'lastName' => 'Ершов', 'secondName' => 'Антонинович'],
+            ['firstName' => 'Лукьян', 'lastName' => 'Гаврилов', 'secondName' => 'Арсеньевич'],
+            ['firstName' => 'Юрий', 'lastName' => 'Поляков', 'secondName' => 'Леонидович'],
+        ];
+    }
+}
